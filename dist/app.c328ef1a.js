d3b32f16bbbb416489e1ed1a274dda553ed54acc
Cameron Moore <cmrnmr@gmail.com>
2020-11-27 16:08:19 -0500

    css

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -40207,7 +40207,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53177" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65397" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
