51b58c1146d8767e838f219bd556718c133a8044
octanym <cmrnmr@gmail.com>
2021-01-07 18:49:56 -0500

    writing project descriptions

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -41589,7 +41589,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58758" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59092" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
