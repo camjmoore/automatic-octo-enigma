6761752fe8137488446c42e9013976944e97cc80
octanym <cmrnmr@gmail.com>
2021-01-08 12:49:05 -0500

    project links working tentative title animations

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -41590,7 +41590,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50033" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52291" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
